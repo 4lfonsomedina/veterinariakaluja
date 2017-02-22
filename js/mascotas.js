501d16cfd3680016d2b71b29d8fc2e4e9ec92683
Alfonso(oficina) <4lfonsomedina@gmail.com>
2017-02-21 16:20:53 -0800

    prueba de script

---
+++ js/mascotas.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/***************Solicitar servicio***********************/
+	$(document).on("click",".sol_serv",function(){
+		var id = this.id.split("_");
+		id=id[1];
+		loading();
+		$.post("http://veterinariakaluja.com/index.php/MascotaController/solicitar_servicio",{id:id,s:""},function(r){
+			$("#modal-header").html("Solicitud de servicio");
+			$("#modal-body").html(r);
+		})
+	})
+})
